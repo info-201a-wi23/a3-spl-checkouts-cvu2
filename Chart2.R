5040867ff3b3d97c65f80d07ef5f47041190ba1e
cvu2 <cvu2@uw.edu>
2023-02-22 15:51:31 -0800

    edited chart 2 rough draft

--- Chart2.R
+++ Chart2.R
@@ -5,3 +5,16 @@ library("scales")
 
 Riordan_df  <- read.csv("~/Desktop/INFO201/Checkouts_by_Title.csv", stringsAsFactors = FALSE)
 
+Riordan_titles <- unique(Riordan_df$Title)
+
+ggplot(data = Top_5_titles)+
+  geom_point(aes(x = CheckoutYear, y = Checkouts))+
+  scale_x_continuous(breaks = seq(2000,2023,2))
+  
+
+Top_5_titles <- Riordan_titles %>% 
+  arrange(desc(Checkouts)) %>% 
+  group_by(Title) %>% 
+  slice(1:5)
+
+
