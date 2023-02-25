faac1494b8d0ea0fceae4f60a751de4691efdbb9
cvu2 <cvu2@uw.edu>
2023-02-24 21:10:11 -0800

    editing chart 2

--- Chart2.R
+++ Chart2.R
@@ -2,19 +2,18 @@
 library("dplyr")
 library("ggplot2")
 library("scales")
+library("stringr")
 
 Riordan_df  <- read.csv("~/Desktop/INFO201/Checkouts_by_Title.csv", stringsAsFactors = FALSE)
 
 Riordan_titles <- unique(Riordan_df$Title)
 
-ggplot(data = Top_5_titles)+
-  geom_point(aes(x = CheckoutYear, y = Checkouts))+
+ggplot(data = Riordan_df)+
+  geom_point(aes(x = CheckoutYear, y = Riordan_titles))+
   scale_x_continuous(breaks = seq(2000,2023,2))
-  
 
-Top_5_titles <- Riordan_titles %>% 
-  arrange(desc(Checkouts)) %>% 
-  group_by(Title) %>% 
-  slice(1:5)
+Percy_Jackson <- str_detect(Riordan_titles, "Percy Jackson")
+Checkouts[str_detect(Riordan_titles, "Percy Jackson")]
 
+View(Percy_Jackson)
 
